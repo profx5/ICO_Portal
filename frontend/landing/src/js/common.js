818c3415ddde76d89eb5ad520f420330a68af4dc
unknown <web.worddoc@gmail.com>
2018-03-04 07:15:52 +0300

    Complete responsive

--- frontend/landing/src/js/common.js
+++ frontend/landing/src/js/common.js
@@ -4,6 +4,23 @@ $(document).ready(function() {
 	// =include classes/*.js
 
 
+  $('.MainSection').height($(window).height());
+  $(window).resize(() => {
+    $('.MainSection').height($(window).height());
+  })
+
+
+
+  $('.Header_menuWrapper').click(function() {
+    $('.Sidebar').addClass('Sidebar-active');
+  })
+
+  $('.Sidebar_menuWrapper').click(function() {
+    $('.Sidebar').removeClass('Sidebar-active');
+  })
+
+
+
   $('.TeamSection_switcher').click(function() {
     $('.TeamSection_switcher').removeClass('TeamSection_switcher-active');
     $('.TeamSection_sliderContainer').removeClass('TeamSection_sliderContainer-active');
@@ -29,21 +46,46 @@ $(document).ready(function() {
         prevEl: '.RoadmapSection_btnPrev',
         nextEl: '.RoadmapSection_btnNext',
       },
-      // centeredSlides: true,
-      // centeredSlides: true,
+      breakpoints: {
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 150,
+        },
+        767: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+          pagination: {
+            el: '.RoadmapSection_pagination',
+            bulletClass: 'RoadmapSection_bullet',
+            bulletActiveClass: 'RoadmapSection_bullet-active'
+          }
+        }
+      },
       initialSlide: 0,
       slideActiveClass: "SectionsSlider_slide-active"
     })
 
+
+
+
     let teamSliderInst = new Swiper($('.TeamSection_sliderContainer'), {
       slidesPerView: 5,
       spaceBetween: 250,
-      // centeredSlides: true,
       navigation: {
         prevEl: '.TeamSection_btnPrev',
         nextEl: '.TeamSection_btnNext',
       },
       // centeredSlides: true,
+      breakpoints: {
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 0,
+        },
+        767: {
+          slidesPerView: 1,
+          spaceBetween: 0
+        }
+      },
       initialSlide: 0,
       slideActiveClass: "SectionsSlider_slide-active",
       scrollbar: {
@@ -53,13 +95,26 @@ $(document).ready(function() {
         dragSize: 45
       }
     })
+
+
+
     let advisiorsSliderInst = new Swiper($('.TeamSection_sliderContainer'), {
       slidesPerView: 5,
       spaceBetween: 250,
-      // centeredSlides: true,
       navigation: {
         prevEl: '.TeamSection_btnPrev',
         nextEl: '.TeamSection_btnNext',
+      },
+      breakpoints: {
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 0,
+
+        },
+        767: {
+          slidesPerView: 1,
+          spaceBetween: 0
+        }
       },
       // centeredSlides: true,
       initialSlide: 0,
@@ -82,33 +137,50 @@ $(document).ready(function() {
 
     map.addMarker(mainMarkerOpts)
 
-
-    // $('[data-fancybox="fancybox"]').click(() => {
       
-      $(`[data-fancybox="fancybox"]`).fancybox({
+    $(`[data-fancybox="fancybox"]`).fancybox({
 
-        idleTime: 0,
-        btnTpl : {
-          smallBtn: `<button data-fancybox-close class="MapPopup_btnClose"></button>`,
+      idleTime: 0,
+      btnTpl : {
+        smallBtn: `<button data-fancybox-close class="MapPopup_btnClose"></button>`,
+      },
+      touch: false,
+      afterLoad: function() {
+
+        var popupMap = new GMaps({
+          div: '.MapPopup_map',
+          lat: 49.282617,
+          lng: -123.092594,
+          zoom: 14,
+          styles: mapStyles,
+        });
+
+        popupMap.addMarker(mainMarkerOpts);
+      },
+    })
+
+    if ($(window).width() < 1025) {
+      
+      let partnersSliderInst = new Swiper(".PartnersSection_slider", {
+        pagination: {
+          el: '.PartnersSection_pagination',
+          bulletClass: 'PartnersSection_bullet',
+          bulletActiveClass: 'PartnersSection_bullet-active'
         },
-        afterLoad: function() {
-
-          var popupMap = new GMaps({
-            div: '.MapPopup_map',
-            lat: 49.282617,
-            lng: -123.092594,
-            zoom: 14,
-            styles: mapStyles,
-          });
-
-          popupMap.addMarker(mainMarkerOpts);
+        breakpoints: {
+          1024: {
+            slidesPerView: 3,
+            slidesPerGroup: 3
+          },
+          767: {
+            slidesPerView: 2,
+            slidesPerGroup: 2
+          }
         },
+        initialSlide: 0,
       })
-    // })
+    }
 
-
-
-    
 
 
 	// =include pages/*.js
