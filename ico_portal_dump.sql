d445a8892e8914a900bae6fe737f389493a65008
Roman Nesytov <r.nesytov@ongrid.pro>
2018-03-15 16:27:05 +0300

    Update dump

--- ico_portal_dump.sql
+++ ico_portal_dump.sql
@@ -153,7 +153,7 @@ CREATE TABLE `auth_user` (
 
 LOCK TABLES `auth_user` WRITE;
 /*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
-INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$100000$XMGY5IFZkKMI$dDTOtcb464oIczKiy4yL4Hd/cfP0FihkIMv8N/vtmAg=','2018-03-13 19:43:49.502302',1,'admin','','','',1,1,'2018-02-27 15:48:35.387168');
+INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$100000$XMGY5IFZkKMI$dDTOtcb464oIczKiy4yL4Hd/cfP0FihkIMv8N/vtmAg=','2018-03-15 16:21:20.850991',1,'admin','','','',1,1,'2018-02-27 15:48:35.387168');
 /*!40000 ALTER TABLE `auth_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -232,7 +232,7 @@ CREATE TABLE `deposits` (
   KEY `deposits_investor_id_d7cc7080_fk_investors_id` (`investor_id`),
   CONSTRAINT `deposits_investor_id_d7cc7080_fk` FOREIGN KEY (`investor_id`) REFERENCES `investors` (`id`),
   CONSTRAINT `deposits_mint_id_19248720_fk_mint_log_id` FOREIGN KEY (`mint_id`) REFERENCES `mint_log` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -241,7 +241,7 @@ CREATE TABLE `deposits` (
 
 LOCK TABLES `deposits` WRITE;
 /*!40000 ALTER TABLE `deposits` DISABLE KEYS */;
-INSERT INTO `deposits` VALUES (16,15.00000000,10.00000000,'2018-03-02 18:35:13.030823',1,23),(17,150.45000000,100.30000000,'2018-03-02 18:36:13.023149',1,24),(18,13383.30000000,8922.20000000,'2018-03-02 18:36:42.975586',1,25),(19,1166.66550000,777.77700000,'2018-03-02 18:38:43.032800',1,26),(20,1166.66550000,777.77700000,'2018-03-02 18:38:43.143106',1,27);
+INSERT INTO `deposits` VALUES (16,15.00000000,10.00000000,'2018-03-02 18:35:13.030823',1,23),(17,150.45000000,100.30000000,'2018-03-02 18:36:13.023149',1,24),(18,13383.30000000,8922.20000000,'2018-03-02 18:36:42.975586',1,25),(19,1166.66550000,777.77700000,'2018-03-02 18:38:43.032800',1,26),(20,1166.66550000,777.77700000,'2018-03-02 18:38:43.143106',1,27),(22,1.33000000,1.00000000,'2018-03-14 16:14:44.604833',4,39);
 /*!40000 ALTER TABLE `deposits` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -266,7 +266,7 @@ CREATE TABLE `django_admin_log` (
   KEY `django_admin_log_user_id_c564eba6_fk` (`user_id`),
   CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
   CONSTRAINT `django_admin_log_user_id_c564eba6_fk` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -275,7 +275,7 @@ CREATE TABLE `django_admin_log` (
 
 LOCK TABLES `django_admin_log` WRITE;
 /*!40000 ALTER TABLE `django_admin_log` DISABLE KEYS */;
-INSERT INTO `django_admin_log` VALUES (1,'2018-02-27 15:49:24.010251','1','Milestone object (1)',1,'[{\"added\": {}}]',7,1),(2,'2018-02-27 15:49:57.996076','2','Milestone object (2)',1,'[{\"added\": {}}]',7,1),(3,'2018-02-27 15:50:26.869981','3','Milestone object (3)',1,'[{\"added\": {}}]',7,1),(4,'2018-02-27 15:52:29.188581','1','First phase',1,'[{\"added\": {}}]',12,1),(5,'2018-02-27 15:53:44.867249','2','Second phase',1,'[{\"added\": {}}]',12,1),(6,'2018-02-27 15:54:10.396732','3','Third phase',1,'[{\"added\": {}}]',12,1),(7,'2018-03-13 18:58:14.695680','6','KYC object (6)',2,'[{\"changed\": {\"fields\": [\"state\"]}}]',13,1),(8,'2018-03-13 19:43:22.355769','8','KYC object (8)',2,'[{\"changed\": {\"fields\": [\"state\"]}}]',13,1),(9,'2018-03-13 19:45:49.470132','9','KYC object (9)',2,'[{\"changed\": {\"fields\": [\"state\"]}}]',13,1),(10,'2018-03-13 19:48:21.355093','10','KYC object (10)',2,'[{\"changed\": {\"fields\": [\"state\"]}}]',13,1);
+INSERT INTO `django_admin_log` VALUES (1,'2018-02-27 15:49:24.010251','1','Milestone object (1)',1,'[{\"added\": {}}]',7,1),(2,'2018-02-27 15:49:57.996076','2','Milestone object (2)',1,'[{\"added\": {}}]',7,1),(3,'2018-02-27 15:50:26.869981','3','Milestone object (3)',1,'[{\"added\": {}}]',7,1),(4,'2018-02-27 15:52:29.188581','1','First phase',1,'[{\"added\": {}}]',12,1),(5,'2018-02-27 15:53:44.867249','2','Second phase',1,'[{\"added\": {}}]',12,1),(6,'2018-02-27 15:54:10.396732','3','Third phase',1,'[{\"added\": {}}]',12,1),(7,'2018-03-13 18:58:14.695680','6','KYC object (6)',2,'[{\"changed\": {\"fields\": [\"state\"]}}]',13,1),(8,'2018-03-13 19:43:22.355769','8','KYC object (8)',2,'[{\"changed\": {\"fields\": [\"state\"]}}]',13,1),(9,'2018-03-13 19:45:49.470132','9','KYC object (9)',2,'[{\"changed\": {\"fields\": [\"state\"]}}]',13,1),(10,'2018-03-13 19:48:21.355093','10','KYC object (10)',2,'[{\"changed\": {\"fields\": [\"state\"]}}]',13,1),(11,'2018-03-13 20:27:40.379056','11','KYC object (11)',2,'[{\"changed\": {\"fields\": [\"state\"]}}]',13,1);
 /*!40000 ALTER TABLE `django_admin_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -318,7 +318,7 @@ CREATE TABLE `django_migrations` (
   `name` varchar(255) COLLATE utf8_bin NOT NULL,
   `applied` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -327,7 +327,7 @@ CREATE TABLE `django_migrations` (
 
 LOCK TABLES `django_migrations` WRITE;
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
-INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2018-02-27 15:45:19.767581'),(2,'auth','0001_initial','2018-02-27 15:45:20.255389'),(3,'admin','0001_initial','2018-02-27 15:45:20.387968'),(4,'admin','0002_logentry_remove_auto_add','2018-02-27 15:45:20.401627'),(5,'contenttypes','0002_remove_content_type_name','2018-02-27 15:45:20.502532'),(6,'auth','0002_alter_permission_name_max_length','2018-02-27 15:45:20.551892'),(7,'auth','0003_alter_user_email_max_length','2018-02-27 15:45:20.606034'),(8,'auth','0004_alter_user_username_opts','2018-02-27 15:45:20.624096'),(9,'auth','0005_alter_user_last_login_null','2018-02-27 15:45:20.666071'),(10,'auth','0006_require_contenttypes_0002','2018-02-27 15:45:20.668516'),(11,'auth','0007_alter_validators_add_error_messages','2018-02-27 15:45:20.687233'),(12,'auth','0008_alter_user_username_max_length','2018-02-27 15:45:20.778187'),(13,'auth','0009_alter_user_last_name_max_length','2018-02-27 15:45:20.825343'),(14,'landing','0001_initial','2018-02-27 15:45:20.858536'),(15,'sessions','0001_initial','2018-02-27 15:45:20.905860'),(16,'user_office','0001_initial','2018-02-27 15:45:21.207528'),(17,'user_office','0002_auto_20180301_2120','2018-03-01 21:20:12.003398'),(18,'user_office','0003_auto_20180301_2205','2018-03-01 22:05:46.798793'),(19,'user_office','0004_auto_20180312_1920','2018-03-12 19:20:26.109187'),(20,'user_office','0005_auto_20180312_1929','2018-03-12 19:29:18.332064'),(21,'user_office','0006_auto_20180312_2105','2018-03-12 21:05:31.064030'),(22,'user_office','0007_kyc','2018-03-13 14:44:35.978810');
+INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2018-02-27 15:45:19.767581'),(2,'auth','0001_initial','2018-02-27 15:45:20.255389'),(3,'admin','0001_initial','2018-02-27 15:45:20.387968'),(4,'admin','0002_logentry_remove_auto_add','2018-02-27 15:45:20.401627'),(5,'contenttypes','0002_remove_content_type_name','2018-02-27 15:45:20.502532'),(6,'auth','0002_alter_permission_name_max_length','2018-02-27 15:45:20.551892'),(7,'auth','0003_alter_user_email_max_length','2018-02-27 15:45:20.606034'),(8,'auth','0004_alter_user_username_opts','2018-02-27 15:45:20.624096'),(9,'auth','0005_alter_user_last_login_null','2018-02-27 15:45:20.666071'),(10,'auth','0006_require_contenttypes_0002','2018-02-27 15:45:20.668516'),(11,'auth','0007_alter_validators_add_error_messages','2018-02-27 15:45:20.687233'),(12,'auth','0008_alter_user_username_max_length','2018-02-27 15:45:20.778187'),(13,'auth','0009_alter_user_last_name_max_length','2018-02-27 15:45:20.825343'),(14,'landing','0001_initial','2018-02-27 15:45:20.858536'),(15,'sessions','0001_initial','2018-02-27 15:45:20.905860'),(16,'user_office','0001_initial','2018-02-27 15:45:21.207528'),(17,'user_office','0002_auto_20180301_2120','2018-03-01 21:20:12.003398'),(18,'user_office','0003_auto_20180301_2205','2018-03-01 22:05:46.798793'),(19,'user_office','0004_auto_20180312_1920','2018-03-12 19:20:26.109187'),(20,'user_office','0005_auto_20180312_1929','2018-03-12 19:29:18.332064'),(21,'user_office','0006_auto_20180312_2105','2018-03-12 21:05:31.064030'),(22,'user_office','0007_kyc','2018-03-13 14:44:35.978810'),(23,'user_office','0008_auto_20180315_1520','2018-03-15 15:20:46.045488');
 /*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -353,7 +353,7 @@ CREATE TABLE `django_session` (
 
 LOCK TABLES `django_session` WRITE;
 /*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
-INSERT INTO `django_session` VALUES ('0fvef1cs2zozt6xvf633nig77lx2icgm','NGUwMjNiZWY3Mjc1NjUwZGZhYjVmMjQyOTdmNGMzYjE5NjA0ZGVmZTp7Il9hdXRoX3VzZXJfaWQiOiIxIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoiZGphbmdvLmNvbnRyaWIuYXV0aC5iYWNrZW5kcy5Nb2RlbEJhY2tlbmQiLCJfYXV0aF91c2VyX2hhc2giOiI5YjA3ZjhiYWRmYWJhMmE5MWYyYzUzZTA0NTQ5ZWE2NWRjMzU1ODkwIn0=','2018-03-27 19:43:49.504513'),('hptbrk33kidglmo6zpjo5vkg61qpi5zj','ZTcwNmRjMDdjOWJlOTAyNTRlZDcwMjQ1NTgyODVlYTM5ZjcxOGE5MDp7Il9hdXRoX3VzZXJfaWQiOiIxIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoidXNlcl9vZmZpY2UuYXV0aF9iYWNrZW5kLlVzZXJPZmZpY2VBdXRoQmFja2VuZCIsIl9hdXRoX3VzZXJfaGFzaCI6IjlkN2MzNzJiYTNlYjgyMDMyOTQxYTU5ZDNhNWE3ZTIzYjc3MzUwY2EifQ==','2018-03-27 19:47:00.827961');
+INSERT INTO `django_session` VALUES ('0fvef1cs2zozt6xvf633nig77lx2icgm','NGUwMjNiZWY3Mjc1NjUwZGZhYjVmMjQyOTdmNGMzYjE5NjA0ZGVmZTp7Il9hdXRoX3VzZXJfaWQiOiIxIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoiZGphbmdvLmNvbnRyaWIuYXV0aC5iYWNrZW5kcy5Nb2RlbEJhY2tlbmQiLCJfYXV0aF91c2VyX2hhc2giOiI5YjA3ZjhiYWRmYWJhMmE5MWYyYzUzZTA0NTQ5ZWE2NWRjMzU1ODkwIn0=','2018-03-27 19:43:49.504513'),('ju5q0oc8my0exox8hjze15tu80d3hru9','ZTcwNmRjMDdjOWJlOTAyNTRlZDcwMjQ1NTgyODVlYTM5ZjcxOGE5MDp7Il9hdXRoX3VzZXJfaWQiOiIxIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoidXNlcl9vZmZpY2UuYXV0aF9iYWNrZW5kLlVzZXJPZmZpY2VBdXRoQmFja2VuZCIsIl9hdXRoX3VzZXJfaGFzaCI6IjlkN2MzNzJiYTNlYjgyMDMyOTQxYTU5ZDNhNWE3ZTIzYjc3MzUwY2EifQ==','2018-03-28 18:55:28.164297'),('lk2p3gpsqk5u62frf43mvr7ez0gvree3','NGUwMjNiZWY3Mjc1NjUwZGZhYjVmMjQyOTdmNGMzYjE5NjA0ZGVmZTp7Il9hdXRoX3VzZXJfaWQiOiIxIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoiZGphbmdvLmNvbnRyaWIuYXV0aC5iYWNrZW5kcy5Nb2RlbEJhY2tlbmQiLCJfYXV0aF91c2VyX2hhc2giOiI5YjA3ZjhiYWRmYWJhMmE5MWYyYzUzZTA0NTQ5ZWE2NWRjMzU1ODkwIn0=','2018-03-29 16:21:20.856312');
 /*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -383,7 +383,7 @@ CREATE TABLE `investors` (
 
 LOCK TABLES `investors` WRITE;
 /*!40000 ALTER TABLE `investors` DISABLE KEYS */;
-INSERT INTO `investors` VALUES ('pbkdf2_sha256$100000$fzJlA0JQ5Ccl$N5uSeqL5Ogz9aAY1pkpDTjp8PrQ14MdAP0iGAFY//LY=','2018-03-13 19:47:00.826427',1,'gordon','0x73015966604928A312F79F7E69291a656Cb88602',32295.91956000),('pbkdf2_sha256$100000$ktjUiRYazK7R$i8g8BWZOmcDckMfawIz1gJkh5ApPAxNSl7gIgf7V64w=','2018-03-13 19:45:12.305980',4,'user1','0xB0a3f48478d84a497f930d8455711d9981B66a70',0.00000000);
+INSERT INTO `investors` VALUES ('pbkdf2_sha256$100000$fzJlA0JQ5Ccl$N5uSeqL5Ogz9aAY1pkpDTjp8PrQ14MdAP0iGAFY//LY=','2018-03-14 18:55:28.162310',1,'gordon','0x73015966604928A312F79F7E69291a656Cb88602',32295.91956000),('pbkdf2_sha256$100000$ktjUiRYazK7R$i8g8BWZOmcDckMfawIz1gJkh5ApPAxNSl7gIgf7V64w=','2018-03-13 19:45:12.305980',4,'user1','0xB0a3f48478d84a497f930d8455711d9981B66a70',1.33000000);
 /*!40000 ALTER TABLE `investors` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -404,10 +404,11 @@ CREATE TABLE `kyc` (
   `document_no` varchar(50) COLLATE utf8_bin NOT NULL,
   `photo` varchar(100) COLLATE utf8_bin NOT NULL,
   `investor_id` int(11) NOT NULL,
+  `country` varchar(30) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `investor_id` (`investor_id`),
   CONSTRAINT `kyc_investor_id_1910e4d7_fk_investors_id` FOREIGN KEY (`investor_id`) REFERENCES `investors` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -467,7 +468,7 @@ CREATE TABLE `mint_log` (
   `txn_hash` varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `mint_log_currency_txn_hash_8dcafacb_uniq` (`currency`,`txn_hash`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -476,7 +477,7 @@ CREATE TABLE `mint_log` (
 
 LOCK TABLES `mint_log` WRITE;
 /*!40000 ALTER TABLE `mint_log` DISABLE KEYS */;
-INSERT INTO `mint_log` VALUES (23,'ETH','0x19bdf30deb1178871b0965aa31a7807e8ea2887b90a0893718285b867f5afe15','0xF69C63e7a39b56b69E09b21496B46005bF950458','0x73015966604928A312F79F7E69291a656Cb88602',10000,'2018-03-02 15:35:12.940424','CONFIRMED',1862325,'0x80ef51753189f2047818feb0c28270f25b2f0a9b3bddc34827e564fa91e3b2af'),(24,'ETH','0x2ca3e05ceacfe704db4cb7f7ce1a11aba2f7bd48118a2918a57bc7e202df08ef','0xF69C63e7a39b56b69E09b21496B46005bF950458','0x73015966604928A312F79F7E69291a656Cb88602',100300,'2018-03-02 15:36:12.942265','CONFIRMED',1862329,'0x7792b9bee82ef5e05c38858ba303d6199801c5dc362343752d847bca39fe38e4'),(25,'ETH','0xe7ddb0a73a6364214d00ae9526620eac40d5aad93e659b9824147b1b21b2f89c','0xF69C63e7a39b56b69E09b21496B46005bF950458','0x73015966604928A312F79F7E69291a656Cb88602',8922200,'2018-03-02 15:36:42.939136','CONFIRMED',1862331,'0xee723c15f00febc8cfdcb617a679097fc0fb2376214983725219817d7de30077'),(26,'ETH','0xa72c855a41006709e340d15640c2b18dc6be332fcaa35f09da1d26ccbb042b49','0xF69C63e7a39b56b69E09b21496B46005bF950458','0x73015966604928A312F79F7E69291a656Cb88602',777777,'2018-03-02 15:38:42.961330','CONFIRMED',1862339,'0x7114c011c9a0c070d39c12e22fcbfef755393c245f2755acaafb4ab582ef74b1'),(27,'ETH','0xa72c855a41006709e340d15640c2b18dc6be332fcaa35f09da1d26ccbb042b49','0xF69C63e7a39b56b69E09b21496B46005bF950458','0x73015966604928A312F79F7E69291a656Cb88602',777777,'2018-03-02 15:38:42.962295','CONFIRMED',1862339,'0x3c45134db6764ceaaf4879f71be6586d59831949be460b8f6d6e479a8acd0e9a');
+INSERT INTO `mint_log` VALUES (23,'ETH','0x19bdf30deb1178871b0965aa31a7807e8ea2887b90a0893718285b867f5afe15','0xF69C63e7a39b56b69E09b21496B46005bF950458','0x73015966604928A312F79F7E69291a656Cb88602',10000,'2018-03-02 15:35:12.940424','CONFIRMED',1862325,'0x80ef51753189f2047818feb0c28270f25b2f0a9b3bddc34827e564fa91e3b2af'),(24,'ETH','0x2ca3e05ceacfe704db4cb7f7ce1a11aba2f7bd48118a2918a57bc7e202df08ef','0xF69C63e7a39b56b69E09b21496B46005bF950458','0x73015966604928A312F79F7E69291a656Cb88602',100300,'2018-03-02 15:36:12.942265','CONFIRMED',1862329,'0x7792b9bee82ef5e05c38858ba303d6199801c5dc362343752d847bca39fe38e4'),(25,'ETH','0xe7ddb0a73a6364214d00ae9526620eac40d5aad93e659b9824147b1b21b2f89c','0xF69C63e7a39b56b69E09b21496B46005bF950458','0x73015966604928A312F79F7E69291a656Cb88602',8922200,'2018-03-02 15:36:42.939136','CONFIRMED',1862331,'0xee723c15f00febc8cfdcb617a679097fc0fb2376214983725219817d7de30077'),(26,'ETH','0xa72c855a41006709e340d15640c2b18dc6be332fcaa35f09da1d26ccbb042b49','0xF69C63e7a39b56b69E09b21496B46005bF950458','0x73015966604928A312F79F7E69291a656Cb88602',777777,'2018-03-02 15:38:42.961330','CONFIRMED',1862339,'0x7114c011c9a0c070d39c12e22fcbfef755393c245f2755acaafb4ab582ef74b1'),(27,'ETH','0xa72c855a41006709e340d15640c2b18dc6be332fcaa35f09da1d26ccbb042b49','0xF69C63e7a39b56b69E09b21496B46005bF950458','0x73015966604928A312F79F7E69291a656Cb88602',777777,'2018-03-02 15:38:42.962295','CONFIRMED',1862339,'0x3c45134db6764ceaaf4879f71be6586d59831949be460b8f6d6e479a8acd0e9a'),(39,'ETH','0x067c18dd6aafccccd88c35b772beaa7a617b6c03f5c952cbe071c7c8415e70d5','0x759DAb92Bdfc423F9eD04725BE489B4D81Bf5f2B','0xB0a3f48478d84a497f930d8455711d9981B66a70',1000,'2018-03-14 13:10:49.821447','CONFIRMED',1930867,'0x291878cad43f503646cb0a02cc61115164abd0fe8599d5532594818e76185616');
 /*!40000 ALTER TABLE `mint_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -516,4 +517,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-03-13 20:16:34
+-- Dump completed on 2018-03-15 16:26:49
