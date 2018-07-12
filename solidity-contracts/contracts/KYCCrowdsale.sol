c329e9eb5d3d86b01437f81d094f17457548cee9
Kirill Varlamov <kirill@ongrid.pro>
2018-07-12 17:32:07 +0300

    Import openzeppelin's safeMath and fix constructor

--- solidity-contracts/contracts/KYCCrowdsale.sol
+++ solidity-contracts/contracts/KYCCrowdsale.sol
@@ -1,51 +1,6 @@
 pragma solidity ^0.4.23;
 
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-
-    /**
-    * @dev Multiplies two numbers, throws on overflow.
-    */
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        assert(c / a == b);
-        return c;
-    }
-
-    /**
-    * @dev Integer division of two numbers, truncating the quotient.
-    */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
-    /**
-    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-    */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    /**
-    * @dev Adds two numbers, throws on overflow.
-    */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
+import "../openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 contract MintableToken {
@@ -123,7 +78,7 @@ contract KYCCrowdsale {
 
     event TokensPurchased(address indexed investor, uint256 weiAmount, uint256 tokens);
 
-    function KYCCrowdsale(address _wallet, MintableToken _token, uint256 _phaseBonus, uint256 _USDcPerETH) public {
+    constructor(address _wallet, MintableToken _token, uint256 _phaseBonus, uint256 _USDcPerETH) public {
         require(_wallet != address(0));
         require(_token != MintableToken(0));
         token = _token;
