2dce1c0345e8e325a5dd6603d3e5ea070dda4e0d
proofx <achievement008@gmail.com>
2018-07-20 10:28:49 +0300

    ICO-162 Password reset pages + signup closed

--- user_office/templatetags/user_office_tags.py
+++ user_office/templatetags/user_office_tags.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.simple_tag
 def site_url():
     return settings.VERA_SITE_URL
+
+
+@register.simple_tag
+def is_signup_open():
+    return not settings.SIGNUP_CLOSED
