72db4510199e8b45fbf734012bf283d6b2098db0
Roman Nesytov <r.nesytov@ongrid.pro>
2018-07-20 18:55:32 +0300

    Add wait db script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+set -e
 echo "Running migrations"
-python manage.py migrate
+
+# wait db
+maxTries=10
+while [ "$maxTries" -gt 0 ] && ! python manage.py migrate; do
+    sleep 1
+done
+echo
+if [ "$maxTries" -le 0 ]; then
+    echo >&2 'error: unable to contact migrate db after 10 tries'
+    exit 1
+fi
 
 echo "Starting Gunicorn"
 gunicorn ico_portal.wsgi:application \
