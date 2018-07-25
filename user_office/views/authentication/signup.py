9a22e44d93bd0353f2502f54a7dde094abff80ba
proofx <achievement008@gmail.com>
2018-07-25 20:16:53 +0300

    ICO-195 Signup password validation

--- user_office/views/authentication/signup.py
+++ user_office/views/authentication/signup.py
@@ -1,43 +1,41 @@
-from django.shortcuts import render
-from django.views.generic import View
 from django.conf import settings
+from django.views.generic import FormView
 from social_core.actions import do_complete
 from social_django.utils import load_strategy, load_backend
 from social_django.views import _do_login
 
-from user_office.models import Investor
 from user_office.forms import SignUpForm
+from user_office.models import Investor
 
 
-class SignUpView(View):
+class SignUpView(FormView):
     template_name = "authentication/signup.html"
+    form_class = SignUpForm
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.request = None
 
     def _get_referrer(self, request):
         if 'refid' in request.POST:
-            referrer = Investor.objects.filter(referral_id=request.POST['refid'])
+            referrer = Investor.objects.filter(referral_id=request.POST.get('refid'))
 
-            if referrer:
-                return referrer[0]
+            if referrer.exists():
+                return referrer.first()
 
-    def get(self, request):
-        form = SignUpForm()
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['sitekey'] = settings.RECAPTCHA_DATA_SITEKEY
+        return ctx
 
-        return render(request, self.template_name, {'form': form,
-                                                    'sitekey': settings.RECAPTCHA_DATA_SITEKEY})
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password2']
 
-    def post(self, request, *args, **kwargs):
-        form = SignUpForm(request.POST)
+        strategy = load_strategy(self.request)
+        backend = load_backend(strategy, 'email', None)
 
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password2']
-
-            strategy = load_strategy(request)
-            backend = load_backend(strategy, 'email', None)
-
-            return do_complete(backend, _do_login,
-                               request=request,
-                               email=email,
-                               password=password)
-        else:
-            raise Exception('Invalid signup data')
+        return do_complete(backend, _do_login,
+                           request=self.request,
+                           email=email,
+                           password=password)
