5a86281cfac01149cc4d2c3fa51e8bd7f0d5a594
Roman Nesytov <r.nesytov@ongrid.pro>
2018-08-19 20:15:51 +0300

    Update travis.yml

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,4 @@ set -x
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build . -t profx5/ico_portal:latest && docker push profx5/ico_portal:latest
+docker build . -t @$ && docker push @$
