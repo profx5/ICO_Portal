17c3d1d28e4ec16c8e4fa8421be87606879a4896
Alexey Krasnov <web.worddoc@gmail.com>
2019-01-31 16:54:20 +0300

    NT-16 Terminate error sources

--- frontend/user_office/src/js/components/layout/Header.js
+++ frontend/user_office/src/js/components/layout/Header.js
@@ -52,7 +52,7 @@ class Header extends React.Component {
                     <NavWrapper>
                         <Logo to="/user_office">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 34" width="26">
-                                <path fill="#3172FD" fill-rule="evenodd" d="M25.7943.4v7.392h-8.64V34h-7.68V7.792h-8.64V.4z"/>
+                                <path fill="#3172FD" fillRule="evenodd" d="M25.7943.4v7.392h-8.64V34h-7.68V7.792h-8.64V.4z"/>
                             </svg>
                         </Logo>
                         <Hamburger onClick={this.onClickHamburgerHandler}>
